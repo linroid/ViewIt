db9a31cf8671c835817ba69b6a91e0ad7f217ea0
linroid <linriod@gmail.com>
2017-01-07 12:50:39 +0800

    init project

---
+++ app/src/main/kotlin/com/linroid/viewit/App.kt
@@ -0,0 +1,13 @@
+package com.linroid.viewit
+
+import android.app.Application
+
+/**
+ * @author linroid <linroid@gmail.com>
+ * @since 07/01/2017
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
